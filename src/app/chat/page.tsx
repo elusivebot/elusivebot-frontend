99fd1ac2b9bb45aa878ee4732ed0df20ed934792
Bryan DeGrendel <bryan@degrendel.com>
2024-07-18 15:57:47 -0400

    Better rendering of text messages
    Use ul+li instead of textarea, plus story them as a typed array.

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -5,8 +5,14 @@ import useWebSocket, { ReadyState } from 'react-use-websocket';
 
 let socket;
 
+interface History {
+  id: string,
+  self: boolean,
+  message: string,
+}
+
 export default function Chat() {
-  const [history, setHistory] = useState('');
+  const [history, setHistory] = useState([]);
   const [message, setMessage] = useState('');
 
   const { sendMessage, lastMessage, readyState } = useWebSocket(process.env.NEXT_PUBLIC_BACKEND_URL);
@@ -20,7 +26,7 @@ export default function Chat() {
   useEffect(() => {
     if (lastMessage?.data) {
       console.log(`Got message ${lastMessage.data}`);
-      setHistory((history) => history.concat(`${lastMessage.data}\n`));
+      setHistory([...history, { id: crypto.randomUUID(), self: false, message: lastMessage.data}]);
     }
   }, [lastMessage]);
 
@@ -28,7 +34,7 @@ export default function Chat() {
   async function onSend(e) {
     e.preventDefault();
     console.log(message);
-    setHistory((history) => history.concat(`> ${message}\n`));
+    setHistory([...history, { id: crypto.randomUUID(), self: true, message: message}]);
     sendMessage(message);
   }
 
@@ -37,9 +43,24 @@ export default function Chat() {
     setMessage(e.target.value);
   }
 
+  function renderHistory(history) {
+    if (history.length) {
+      const items = history.map(entry => {
+        if (entry.self) {
+          return (<li key={entry.id}>> {entry.message}</li>)
+        } else {
+          return (<li key={entry.id} className="text-right">{entry.message}</li>)
+        }
+      });
+      return <ul>{items}</ul>
+    } else {
+      return <p>...</p>
+    }
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <textarea readOnly value={history} className="text-gray-900" />
+      <div className="text-white-900">{renderHistory(history)}</div>
       <input type="text" value={message} onChange={onMessageChange} className="text-gray-900"
         onKeyDown={(e) => {
           if (e.key === 'Enter') {
